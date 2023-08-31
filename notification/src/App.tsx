090443375bf6f3c7fbe9f995ec96275800e95ef8
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-09-01 03:46:46 +0700

    Innitial Notification app

---
+++ notification/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style/main.css'
+import 'jquery-ui-bundle';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'jquery-nice-select';
+
+const App = () => {
+
+  return (
+    <div className='container'>
+      <h1>Notification app</h1>
+    </div>
+  );
+}
+
+export default App;
